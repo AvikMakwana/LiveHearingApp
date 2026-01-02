06417bb7385aff3b5e02d4455293c2e3011afc4b
AvikMakwana <avikmakwana81@gmail.com>
2026-01-02 16:05:57 +0530

    chore: Update app version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.avikmakwana.livehearingapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
